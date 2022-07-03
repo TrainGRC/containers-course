0d00ac4667a1da7d415327a276d7162ea76c0285
Wes Ladd <wsladd@icloud.com>
2022-07-03 07:05:42 -0500

    add trivy binary

--- image-1/run.sh
+++ image-1/run.sh
@@ -1,5 +1,7 @@
-echo "Hello" $MY_NAME
-echo "Welcome to your very own container image."
+echo "Hello, "$MY_NAME"."
+echo ""
+sleep 2
+echo "Welcome to your very own container image. This container image was built just for you..."
 echo ""
 sleep 3
 echo "Wait! That isn't your name! - sorry."
@@ -10,4 +12,4 @@ echo "If you want make this image your own - all you have to do is update the EN
 echo ""
 sleep 3
 echo "Then...rebuild the image - remember, container images are immutable!"
-sleep 7
+sleep 3
