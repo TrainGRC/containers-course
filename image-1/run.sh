cb6f8ba3020e31cc21c49b1ad3985b2a45faa3eb
Wes <wsladd@icloud.com>
2022-07-03 10:40:05 -0500

    image-1 updates

--- image-1/run.sh
+++ image-1/run.sh
@@ -4,12 +4,17 @@ sleep 2
 echo "Welcome to your very own container image. This container image was built just for you..."
 echo ""
 sleep 3
-echo "Wait! That isn't your name! - sorry."
-echo ""
-sleep 3
-
-echo "If you want make this image your own - all you have to do is update the ENV variable MY_NAME in the Dockerfile"
-echo ""
-sleep 3
-echo "Then...rebuild the image - remember, container images are immutable!"
+DEFAULT_NAME='John Jacob Jingleheimer Schmidt'
+if [[ "$DEFAULT_NAME" == "$MY_NAME" ]]
+    then
+        echo "Wait! That isn't your name! - sorry."
+        sleep 3
+        echo ""
+        echo "If you want make this image your own - all you have to do is update the ENV variable MY_NAME in the Dockerfile"
+        echo ""
+        sleep 3
+        echo "Then...rebuild the image - remember, container images are immutable!"
+    else
+        echo "I hope you enjoy your very own image."
+fi
 sleep 3
